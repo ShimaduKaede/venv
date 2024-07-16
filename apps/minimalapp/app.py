84dcefc77ab542c71fa2ce3ce4701ced4aa034d1
AkatukiKaede <ooi2372054@stu.o-hara.ac.jp>
2024-07-16 11:49:08 +0900

    contactformの追加

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,request,url_for,redirect
 
 app=Flask(__name__)
 
@@ -19,3 +19,19 @@ def hello(name):
 @app.route("/name/<name>")
 def show_name(name):
     return render_template("index.html",name=name)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/contact/complate",methods =["GET","POST"])
+def contact_complate():
+    if request.method=="POST":
+        # メール送信
+        
+        # contactエンドポイントまでリダイレクト
+        return redirect(url_for("contact_complate"))
+    else:
+        return render_template("contact_complate.html")
