d21056be830090468eeadf38e358b25f0b4a68da
AkatukiKaede <ooi2372054@stu.o-hara.ac.jp>
2024-07-16 10:12:37 +0900

    templates追加

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app=Flask(__name__)
 
@@ -13,3 +14,8 @@ def index():
 @app.route("/hello/<name>",methods=["POST","GET"],endpoint="hello-endpoint")
 def hello(name):
     return f"Hello,{name}!"
+
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html",name=name)
