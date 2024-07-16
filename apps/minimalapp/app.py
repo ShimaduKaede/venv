17fba9f579903e50578e8a7693f7183166d8175a
AkatukiKaede <ooi2372054@stu.o-hara.ac.jp>
2024-07-16 13:45:42 +0900

    contact エラー文追記

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask,flash
 from flask import render_template,request,url_for,redirect
+from email_validator import validate_email,EmailNotValidError
+
 
 app=Flask(__name__)
+    # SECRET_KEYを追加する
+app.config['SECRET_KEY'] ="2AZSMss3p5QPbcY2hBsJ"
 
 if __name__=="__main__":
     app.run(debug=True)
@@ -33,9 +37,32 @@ def contact_complate():
         username=request.form["username"]
         email=request.form["email"]
         description=request.form["description"]
+        
+        # 入力チェック
+        is_valid=True
+        if not username:
+            flash("ユーザー名は必須です")
+            is_valid=False
+        
+        if not email:
+            flash("メールアドレスは必須です")
+            is_valid=False
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("メールアドレスの形式で入力してください")
+            is_valid=False
+            
+        if not description:
+            flash("お問い合わせ内容は必須です")
+            is_valid=False
+            
+        if not is_valid:
+            return redirect(url_for("contact"))
         # メール送信
         
         # contactエンドポイントまでリダイレクト
+        flash("お問い合わせ内容はメールにて送信しました。お問い合わせありがとうございました。")
         return redirect(url_for("contact_complate"))
     else:
         return render_template("contact_complate.html")
