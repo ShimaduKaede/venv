6c78b85958ed4c29a344021b857255ec13227352
AkatukiKaede <ooi2372054@stu.o-hara.ac.jp>
2024-07-17 10:04:59 +0900

    sqlalchemyのセットアップ

--- apps/app.py
+++ apps/app.py
@@ -1,9 +1,25 @@
+from pathlib import Path
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+
+db=SQLAlchemy()
 
 # create_app関数を作成する
 def create_app():
     # Flaskインスタンス生成
     app = Flask(__name__)
+    # アプリのコンフィグ設定
+    app.config.from_mapping(
+        SECRET_KEY='2AZSMss3p5QPbcY2hBj',
+        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent / 'local.sqlite'}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+    )
+    # sqlalchemyとアプリを連携
+    db.init_app(app)
+    # migrateとアプリを連携
+    Migrate(app,db)
     
     app.run(debug=True)
     
